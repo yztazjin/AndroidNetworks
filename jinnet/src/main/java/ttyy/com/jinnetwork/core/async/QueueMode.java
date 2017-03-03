5d2eadafd03a2e23a99f3f7610a0f22ad5d3182d
yztazjin <hudadada0305@gmail.com>
2017-03-03 09:38:02 +0800

    Add files via upload

---
+++ jinnet/src/main/java/ttyy/com/jinnetwork/core/async/QueueMode.java
@@ -0,0 +1,22 @@
+package ttyy.com.jinnetwork.core.async;
+
+/**
+ * author: admin
+ * date: 2017/03/03
+ * version: 0
+ * mail: secret
+ * desc: QueueMode
+ */
+
+public enum QueueMode {
+
+    /**
+     * 先进先出
+     */
+    FirstInFirstOut,
+    /**
+     * 后进先出
+     */
+    LastInFirstOut;
+
+}
