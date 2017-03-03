6f63434a355d0db836934080ddefd945292f4e56
yztazjin <hudadada0305@gmail.com>
2017-03-03 19:01:18 +0800

    Update Https.java

--- jinnet/src/main/java/ttyy/com/jinnetwork/Https.java
+++ jinnet/src/main/java/ttyy/com/jinnetwork/Https.java
@@ -1,19 +1,11 @@
 package ttyy.com.jinnetwork;
 
-import android.graphics.Bitmap;
-import android.util.Log;
-
-import java.io.File;
 import java.lang.reflect.Proxy;
 
-import ttyy.com.jinnetwork.core.async.$HttpExecutorPool;
 import ttyy.com.jinnetwork.core.config.HttpConfig;
 import ttyy.com.jinnetwork.core.work.method_get.HttpRequestGetBuilder;
 import ttyy.com.jinnetwork.core.work.method_post.HttpRequestPostBuilder;
 import ttyy.com.jinnetwork.core.work.method_post.PostContentType;
-import ttyy.com.jinnetwork.ext_imgloader.ImageCache;
-import ttyy.com.jinnetwork.ext_imgloader.TargetWrapper;
-import ttyy.com.jinnetwork.ext_imgloader.LoaderConfig;
 import ttyy.com.jinnetwork.ext_reflect.APIRequestProxy;
 
 /**
@@ -79,48 +71,6 @@ public class Https {
         APIRequestProxy proxy = new APIRequestProxy();
 
         return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, proxy);
-    }
-
-    /**
-     * 加载图片
-     * @param url
-     * @param target
-     */
-    public static void loadImg(String url, TargetWrapper target) {
-
-        if (url == null) {
-            Log.d("Https", "url is empty");
-            return;
-        }
-
-        if (target != null) {
-            target.getTargetView().setTag(url.hashCode());
-        }
-
-        HttpRequestGetBuilder builder = get(url)
-                .setRequestClient(LoaderConfig.get().getRequestClient())
-                .setHttpCallback(target);
-
-        if(ImageCache.get().isRuntimeCacheHit(builder.build())){
-            // 内存缓存拿到了
-            Bitmap bm = ImageCache.get().getRuntimeCache(url.hashCode());
-            target.setResponse(bm);
-            return;
-        }
-        
-        if (url.startsWith("file://")) {
-            url = url.substring(7);
-            File mResponseFile = new File(url);
-            builder.setResponseFile(mResponseFile);
-        }
-
-        File cacheDir = LoaderConfig.get().getCacheDir();
-        if (cacheDir != null) {
-            builder.setDownloadMode(new File(cacheDir, String.valueOf(url.hashCode())));
-        }
-
-        builder.build().$async($HttpExecutorPool.get().getImgExecutor());
-
     }
 
 }
