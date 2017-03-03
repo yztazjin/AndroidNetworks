fc29140197c9475dfec7ccc09f3d2de686096f80
yztazjin <hudadada0305@gmail.com>
2017-03-03 14:49:27 +0800

    Update HttpRequestBuilder.java

--- jinnet/src/main/java/ttyy/com/jinnetwork/core/work/HttpRequestBuilder.java
+++ jinnet/src/main/java/ttyy/com/jinnetwork/core/work/HttpRequestBuilder.java
@@ -7,6 +7,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
@@ -45,177 +46,215 @@ public class HttpRequestBuilder {
      * 响应文件
      * 如果响应文件不为空，那么就不会从网络请求数据，直接从该文件获取数据
      */
-    protected byte[] mResponseStreamBytes;
+    protected File mResponseStreamFile;
 
-    protected HttpRequestBuilder(){
+    protected HttpRequestBuilder() {
         mClientType = HttpConfig.get().getClientType();
     }
 
-    public HttpRequestBuilder addParam(String key, String value){
+    public HttpRequestBuilder addParam(String key, String value) {
         params.put(key, value);
         return this;
     }
 
-    public HttpRequestBuilder addParam(String key, int value){
+    public HttpRequestBuilder addParam(String key, int value) {
         params.put(key, value);
         return this;
     }
 
-    public HttpRequestBuilder addParam(String key, float value){
+    public HttpRequestBuilder addParam(String key, float value) {
         params.put(key, value);
         return this;
     }
 
-    public HttpRequestBuilder addParam(String key, long value){
+    public HttpRequestBuilder addParam(String key, long value) {
         params.put(key, value);
         return this;
     }
 
-    public HttpRequestBuilder addParam(String key, double value){
+    public HttpRequestBuilder addParam(String key, double value) {
         params.put(key, value);
         return this;
     }
 
-    public HttpRequestBuilder addHeader(String key, String value){
+    public HttpRequestBuilder addHeader(String key, String value) {
         headers.put(key, value);
         return this;
     }
 
-    public HttpRequestBuilder removeHeader(String key){
+    public HttpRequestBuilder removeHeader(String key) {
         headers.remove(key);
         return this;
     }
 
-    public HttpRequestBuilder removeParam(String key){
+    public HttpRequestBuilder removeParam(String key) {
         params.remove(key);
         return this;
     }
 
-    public Map<String, String> getHeadersDict(){
+    public Map<String, String> getHeadersDict() {
         return headers;
     }
 
-    public Map<String, Object> getParasmDict(){
+    public Map<String, Object> getParasmDict() {
         return params;
     }
 
-    public HttpRequestBuilder setRequestURL(String url){
+    public HttpRequestBuilder setRequestURL(String url) {
         mRequestURL = url;
         return this;
     }
 
     /**
      * 设置自定义的网络请求客户端
+     *
      * @param client
      * @return
      */
-    public HttpRequestBuilder setRequestClient(Client client){
+    public HttpRequestBuilder setRequestClient(Client client) {
         mRequestClient = client;
         return this;
     }
 
     /**
      * 获取自定义的网络客户端
+     *
      * @return
      */
-    public Client getRequestClient(){
+    public Client getRequestClient() {
         return mRequestClient;
     }
 
-    public String getRequestURL(){
+    public String getRequestURL() {
         return mRequestURL;
     }
 
-    public HttpRequestBuilder setHttpCallback(HttpCallback callback){
-        if(callback instanceof HttpUIThreadCallbackAdapter){
+    public HttpRequestBuilder setHttpCallback(HttpCallback callback) {
+        if (callback instanceof HttpUIThreadCallbackAdapter) {
             mCallback = $HttpUIThreadCallbackAdapterProxy.get(callback);
-        }else {
+        } else {
             mCallback = callback;
         }
         return this;
     }
 
-    public HttpCallback getHttpCallback(){
+    public HttpCallback getHttpCallback() {
         return mCallback;
     }
 
-    public File getDownloadFile(){
+    public File getDownloadFile() {
         return null;
     }
 
-    public HttpRequestBuilder setResponseStream(InputStream is){
+    public HttpRequestBuilder setResponseStream(InputStream is, File tmpFile) {
+        if (tmpFile != null) {
 
-        byte[] buffer = new byte[4096];
-        int length = 0;
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try {
-            while ((length = is.read(buffer))!=-1){
-                baos.write(buffer, 0, length);
+            try {
+                if (!tmpFile.exists()) {
+                    tmpFile.getParentFile().mkdirs();
+                    tmpFile.createNewFile();
+                }
+
+                byte[] buffer = new byte[4096];
+                int length = 0;
+                FileOutputStream fos = new FileOutputStream(tmpFile);
+                while ((length = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, length);
+                }
+                is.close();
+                fos.flush();
+                fos.close();
+
+                setResponseFile(tmpFile);
+            } catch (IOException e) {
+                Log.w("Https", "setResponseStream io exception file not exists");
             }
-            is.close();
-            mResponseStreamBytes = baos.toByteArray();
-        } catch (IOException e) {
-            Log.w("Https", "io exception");
         }
-
         return this;
     }
 
     /**
      * 设置自定义的响应文件
      * 设置了该字段，数据不会走网络请求
+     *
      * @param file
      * @return
      */
-    public HttpRequestBuilder setResponseFile(File file){
-        try {
-            return setResponseStream(new FileInputStream(file));
-        } catch (FileNotFoundException e) {
-            Log.w("Https", "response file not exists!");
-        }
+    public HttpRequestBuilder setResponseFile(File file) {
+        mResponseStreamFile = file;
         return this;
     }
 
     /**
      * 包装自定义的响应流
+     *
      * @return
      */
-    public InputStream getResponseStream(){
-        if(mResponseStreamBytes == null)
+    public InputStream getResponseStream() {
+        if (mResponseStreamFile == null)
             return null;
-        return new ByteArrayInputStream(mResponseStreamBytes);
+        try {
+            return new FileInputStream(mResponseStreamFile);
+        } catch (FileNotFoundException e) {
+            Log.w("Https", "getResponseStream io exception file not exists");
+        }
+
+        return null;
     }
 
     /**
      * 获取响应流的数据 二进制
+     *
      * @return
      */
-    public byte[] getResponseStreamBytes(){
+    public byte[] getResponseStreamBytes() {
+        InputStream is = getResponseStream();
+        byte[] mResponseStreamBytes = new byte[]{};
+        if (is == null) {
+            return mResponseStreamBytes;
+        }
+        byte[] buffer = new byte[4096];
+        int length = 0;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            while ((length = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, length);
+            }
+            is.close();
+            baos.flush();
+            baos.close();
+
+            mResponseStreamBytes = baos.toByteArray();
+        } catch (IOException e) {
+            Log.w("Https", "getResponseStreamBytes io exception");
+        }
         return mResponseStreamBytes;
     }
 
     /**
      * 获取网络请求客户端类型
+     *
      * @return
      */
-    public ClientType getClientType(){
+    public ClientType getClientType() {
         return mClientType;
     }
 
     /**
      * 获取post的contenttype
+     *
      * @return
      */
-    public PostContentType getContentType(){
+    public PostContentType getContentType() {
         return null;
     }
 
-    public HttpRequestBuilder setClientType(ClientType type){
+    public HttpRequestBuilder setClientType(ClientType type) {
         mClientType = type;
         return this;
     }
 
-    public HTTPRequest build(){
+    public HTTPRequest build() {
         return new HTTPRequest(this);
     }
 }
