ff428c2e2f500021bebffd0c8a1a1eed2035acec
yztazjin <hudadada0305@gmail.com>
2017-03-04 13:52:52 +0800

    Update HttpImageRequest.java

--- jinnet/src/main/java/ttyy/com/jinnetwork/ext_image/HttpImageRequest.java
+++ jinnet/src/main/java/ttyy/com/jinnetwork/ext_image/HttpImageRequest.java
@@ -32,6 +32,16 @@ class HttpImageRequest extends HTTPRequest {
     public HTTPResponse request() {
         HTTPResponse rsp = null;
         String uri = getRequestURL();
+
+        // 是否启用缓存
+        if (mImageBuilder.isUseCache()
+                && ((ViewTracker)getHttpCallback()).isViewTracked()) {
+            rsp = responseFromCache();
+            if(rsp != null){
+                return rsp;
+            }
+        }
+
         if (uri.startsWith("file://")) {
             // 本地文件
             File mResponseFile = builder.getResponseStreamFile();
@@ -54,47 +64,59 @@ class HttpImageRequest extends HTTPRequest {
     @Override
     public void requestAsync() {
 
-        String uri = getRequestURL();
         // 是否启用缓存
-        Log.d("Images", "isUseCache "+mImageBuilder.isUseCache());
         if (mImageBuilder.isUseCache()) {
-            boolean isRuntimeCacheHit = ImageCache.getInstance().isRuntimeCacheHit(uri);
-            Log.d("Images", "isRuntimeCacheHit "+isRuntimeCacheHit);
-            if(isRuntimeCacheHit){
-                getHttpCallback().onPreStart(this);
-
-                ViewTracker tracker = (ViewTracker) getHttpCallback();
-
-                Bitmap bm = ImageCache.getInstance().getRuntimeCache(uri);
-                ImageCache.getInstance().setIntoCache(uri, bm);
-
-                $HttpResponse rsp = new $HttpResponse(this);
-                rsp.setStatusCode(200);
-                rsp.setContentLength(bm.getRowBytes() * bm.getHeight());
-
-                tracker.setImageIntoView(bm);
-                tracker.onFinish(rsp);
-
-            }else {
-                boolean isDiskCacheHit = ImageCache.getInstance().isDiskCacheHit(uri);
-                Log.d("Images", "isDiskCacheHit "+isDiskCacheHit);
-                if(isDiskCacheHit){
-
-                    File sourceFile = ImageCache.getInstance().getDiskCache(uri);
-                    builder.setResponseFile(sourceFile);
-                    $HttpResponse rsp = new $HttpResponse(this);
-                    rsp.setStatusCode(200);
-                    rsp.setContentLength(builder.getResponseStreamFile().length());
-
-                    getHttpCallback().onSuccess(rsp);
-                    getHttpCallback().onFinish(rsp);
-
-                    return;
-                }
+            if(responseFromCache() != null){
+                return;
             }
-
         }
 
         $HttpExecutorPool.get().getImgExecutor().addRequest(this).start();
+    }
+
+    /**
+     * 从缓存中获取响应
+     * @return
+     */
+    private HTTPResponse responseFromCache(){
+
+        String uri = getRequestURL();
+        boolean isRuntimeCacheHit = ImageCache.getInstance().isRuntimeCacheHit(uri);
+        Log.i("Images", "isRuntimeCacheHit "+isRuntimeCacheHit);
+        if(isRuntimeCacheHit){
+            getHttpCallback().onPreStart(this);
+
+            ViewTracker tracker = (ViewTracker) getHttpCallback();
+
+            Bitmap bm = ImageCache.getInstance().getRuntimeCache(uri);
+            ImageCache.getInstance().setIntoCache(uri, bm);
+
+            $HttpResponse rsp = new $HttpResponse(this);
+            rsp.setStatusCode(200);
+            rsp.setContentLength(bm.getRowBytes() * bm.getHeight());
+
+            tracker.setImageIntoView(bm);
+            tracker.onFinish(rsp);
+
+            return rsp;
+        }else {
+            boolean isDiskCacheHit = ImageCache.getInstance().isDiskCacheHit(uri);
+            Log.i("Images", "isDiskCacheHit "+isDiskCacheHit);
+            if(isDiskCacheHit){
+
+                File sourceFile = ImageCache.getInstance().getDiskCache(uri);
+                builder.setResponseFile(sourceFile);
+                $HttpResponse rsp = new $HttpResponse(this);
+                rsp.setStatusCode(200);
+                rsp.setContentLength(builder.getResponseStreamFile().length());
+
+                getHttpCallback().onSuccess(rsp);
+                getHttpCallback().onFinish(rsp);
+
+                return rsp;
+            }
+        }
+
+        return null;
     }
 }
