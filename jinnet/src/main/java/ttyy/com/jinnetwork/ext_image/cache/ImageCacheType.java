e53ba095d31fb7cce164cac0e2ea04f511aacd78
yztazjin <hudadada0305@gmail.com>
2017-03-21 14:07:06 +0800

    Add files via upload

---
+++ jinnet/src/main/java/ttyy/com/jinnetwork/ext_image/cache/ImageCacheType.java
@@ -0,0 +1,26 @@
+package ttyy.com.jinnetwork.ext_image.cache;
+
+/**
+ * author: admin
+ * date: 2017/03/21
+ * version: 0
+ * mail: secret
+ * desc: ImageCacheType
+ */
+
+public enum ImageCacheType {
+
+    /**
+     * 内存缓存
+     */
+    RuntimeCache,
+    /**
+     * SDCard缓存
+     */
+    DiskCache,
+    /**
+     * 所有类型缓存
+     */
+    AllCache;
+
+}
